67522306a42b215075701b3728e4a56ac2f0300c
Isabella Bazzi <101848020+isabellabazzi@users.noreply.github.com>
2023-03-02 19:52:15 -0300

    create func exibir & remove "oi"

--- index.py
+++ index.py
@@ -1,1 +1,10 @@
+dicionario = {1234:["Isabella", "Bazzi"]}
+
+def exibir():
+  
+for chave, lista in dicionario.items():
+  print("código.....", lista[0])
+  print("nome.....", lista[1])
+  print("cargo.....", lista[2])
+  
 print ("oi");
