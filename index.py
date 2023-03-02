85790e4a00ce46f652797593a9e51c6e45725ab7
Isabella Bazzi <101848020+isabellabazzi@users.noreply.github.com>
2023-03-02 19:52:51 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,8 +1,7 @@
 dicionario = {1234:["Isabella", "Bazzi"]}
 
 def exibir():
-  
-for chave, lista in dicionario.items():
+  for chave, lista in dicionario.items():
   print("código.....", lista[0])
   print("nome.....", lista[1])
   print("cargo.....", lista[2])
